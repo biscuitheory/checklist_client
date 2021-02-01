b5a37eb48daf7d4517dd2df24dbbc5bd4d619a22
Sylvie <sylvie.z.lu@gmail.com>
2021-02-01 18:52:36 +0100

    :sparkles: updated signup form logic w/ customedhook validator

---
+++ src/components/customedhooks/useForm.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+
+const useForm = (initialState, validate, callback) => {
+  const [values, setValues] = useState(initialState);
+  const [errors, setErrors] = useState({});
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setValues({
+      ...values,
+    });
+    setErrors(validate(values));
+    setIsSubmitting(true);
+  };
+
+  // console.log('from useform', values);
+
+  useEffect(() => {
+    if (Object.keys(errors).length === 0 && isSubmitting) {
+      callback();
+    }
+  }, [errors, isSubmitting]);
+
+  return {
+    handleChange,
+    handleSubmit,
+    values,
+    setValues,
+    errors,
+  };
+};
+
+export default useForm;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,20 +1,59 @@
 import React from 'react';
+import axios from 'axios';
 
 import WelcomeTopIcon from '../components/layout/WelcomeTopIcon';
 import SignButton from '../components/buttons/SignButton';
 import SignButtonDivider from '../components/buttons/SignButtonDivider';
 
 import usePasswordToggle from '../components/customedhooks/usePasswordToggle';
+import useForm from '../components/customedhooks/useForm';
+import validate from '../components/validators/validateSignup';
+
+const API = process.env.REACT_APP_DEV_API_URL;
 
 const Signup = () => {
   const [PasswordInputType, ToggleIcon] = usePasswordToggle();
+
+  const initialState = {
+    firstname: '',
+    lastname: '',
+    email: '',
+    password: '',
+  };
+
+  const { handleChange, handleSubmit, values, setValues, errors } = useForm(
+    initialState,
+    validate,
+    submit
+  );
+
+  async function submit() {
+    try {
+      const res = await axios.post(`${API}signup`, {
+        firstname: values.firstname,
+        lastname: values.lastname,
+        email: values.email,
+        password: values.password,
+      });
+
+      if (res.status === 201) {
+        console.log('signup success', res);
+      }
+    } catch (err) {
+      console.log('error from signup', err);
+      setValues({
+        ...values,
+        errorMessage: err.message,
+      });
+    }
+  }
 
   return (
     <div className="welcome__container">
       <div className="welcome__container-leftpanel">
         <WelcomeTopIcon />
         <div className="welcome__container-leftpanel-bottomhalf">
-          <form className="form__container">
+          <form onSubmit={handleSubmit} noValidate className="form__container">
             <h2 className="form__container-title">Create an account</h2>
             <label htmlFor="firstname" className="form__container-label">
               Firstname
@@ -25,9 +64,12 @@ const Signup = () => {
                   type="firstname"
                   name="firstname"
                   placeholder="firstname"
+                  value={values.firstname}
+                  onChange={handleChange}
                   className="form__container-input"
                 />
               </div>
+              {errors.firstname && <p className="error">{errors.firstname}</p>}
             </label>
             <label htmlFor="lastname" className="form__container-label">
               Lastname
@@ -38,9 +80,12 @@ const Signup = () => {
                   type="lastname"
                   name="lastname"
                   placeholder="lastname"
+                  value={values.lastname}
+                  onChange={handleChange}
                   className="form__container-input"
                 />
               </div>
+              {errors.lastname && <p className="error">{errors.lastname}</p>}
             </label>
             <label htmlFor="email" className="form__container-label">
               Email
@@ -51,9 +96,12 @@ const Signup = () => {
                   type="email"
                   name="email"
                   placeholder="name@domain.com"
+                  value={values.email}
+                  onChange={handleChange}
                   className="form__container-input"
                 />
               </div>
+              {errors.email && <p className="error">{errors.email}</p>}
             </label>
             <label htmlFor="password" className="form__container-label">
               Password
@@ -64,10 +112,13 @@ const Signup = () => {
                   type={PasswordInputType}
                   name="password"
                   placeholder="at least 8 characters"
+                  value={values.password}
+                  onChange={handleChange}
                   className="form__container-input"
                 />
                 <span className="password-toggle-icon">{ToggleIcon}</span>
               </div>
+              {errors.password && <p className="error">{errors.password}</p>}
             </label>
             <span className="welcome__container-leftpanel-bottomhalf-buttons">
               <SignButton value="signup">Sign up</SignButton>
